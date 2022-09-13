341232ca8bb8c785ca4f4bc509e13a520325b4a9
Vyacheslav Nikolaevich <slavakorablev12@gmail.com>
2022-09-13 23:44:43 +0500

    add hello Masha

---
+++ examples/example005diffconditionfls/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Edit user name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+   Console.WriteLine("Ура, это же маша");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
